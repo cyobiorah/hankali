fba31a094955a5ef54c835c504156b957eced23d
Obiorah Cyril <obiorah@softcom.ng>
2020-02-11 07:48:15 +0100

    checking heroku deploy

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+
+// // Serve static files....
+// app.use(express.static(__dirname + '/dist/hankali'));
+
+// // Send all requests to index.html
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname + '/dist/hankali/index.html'));
+// });
+
+// // default Heroku PORT
+// app.listen(process.env.PORT || 3000);
+
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-// Serve static files....
-app.use(express.static(__dirname + '/dist/hankali'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/hankali'));
 
-// Send all requests to index.html
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/hankali/index.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/dist/hankali/index.html'));
 });
 
-// default Heroku PORT
-app.listen(process.env.PORT || 3000);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
